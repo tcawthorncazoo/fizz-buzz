cb69dcd6f033d8987ca661f022708690789166bf
Thomas Cawthorn <thomas.cawthorn@cazoo.co.uk>
2021-02-02 15:23:49 +0000

    🔴🔴🔴🔴 RED - Create fizz buzz function - to squash

--- src/fizz-buzz.spec.ts
+++ src/fizz-buzz.spec.ts
@@ -1,5 +1,9 @@
 import { expect } from "chai";
 
+function fizzBuzz(number: number): string {
+
+}
+
 describe('Fizz Buzz', () => {
     it('should return "4" when 4', () => {
         expect(fizzBuzz(4)).eql('4');
