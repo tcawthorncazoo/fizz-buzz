9a9a03fe16b1b2503fe0ad591703b492b4e81805
Thomas Cawthorn <thomas.cawthorn@cazoo.co.uk>
2021-02-02 15:29:28 +0000

    🔴🔴🔴🔴 RED - 1 should return '1' - to squash

--- src/fizz-buzz.spec.ts
+++ src/fizz-buzz.spec.ts
@@ -5,6 +5,10 @@ function fizzBuzz(number: number): string {
 }
 
 describe('Fizz Buzz', () => {
+    it('should return "1" when 1', () => {
+        expect(fizzBuzz(1)).eql('1');
+    });
+
     it('should return "4" when 4', () => {
         expect(fizzBuzz(4)).eql('4');
     });
