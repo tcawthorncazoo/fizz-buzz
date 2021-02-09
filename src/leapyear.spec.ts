ce3b427d4f01b9af283e2f1d8910616cbf45099a
Thomas Cawthorn <thomas.cawthorn@cazoo.co.uk>
2021-02-09 16:32:16 +0000

    mob next [ci-skip]

--- src/leapyear.spec.ts
+++ src/leapyear.spec.ts
@@ -1,15 +1,24 @@
 import { expect } from "chai";
 
 function isLeapYear(year: number) {
-    if (year === 1997 || year === 2007) {
-        return false;
+    if (year === 1996 || year === 2000) {
+        return true;
     }
-    return true;
+    return false;
 }
+
+/**
+ * A leap year is defined as one that is divisible by 4, but is not otherwise divisible by 100 unless it is also divisible by 400.
+ * For example, 2001 is a typical common year and 1996 is a typical leap year, whereas 1900 is an atypical common year and 2000 is an atypical leap year.
+ */
 
 describe('Leap Year', () => {
     it('should return TRUE for 1996', () => {
         expect(isLeapYear(1996)).eql(true);
+    });
+
+    it('should return TRUE for 1992', () => {
+        expect(isLeapYear(1992)).eql(true);
     });
 
     it('should return TRUE for 2000', () => {
