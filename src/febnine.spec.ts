d510053e1300735a7c7f8719ca1a53a18a676435
Thomas Cawthorn <thomas.cawthorn@cazoo.co.uk>
2021-02-09 15:38:50 +0000

    mob next [ci-skip]

--- src/febnine.spec.ts
+++ src/febnine.spec.ts
@@ -1,6 +1,9 @@
 import { expect } from "chai";
 
 function fizzBuzz(input: number): string {
+    if (input === 2) {
+        return "2";
+    }
     return "1";
 }
 
@@ -11,6 +14,9 @@ describe('Fizz Buzz on 09 Feb', () => {
 
     it('should return "2" when 2', () => {
         expect(fizzBuzz(2)).eql('2');
-    })
+    });
 
+    it('should return "4" when 4', () => {
+        expect(fizzBuzz(4)).eql('4');
+    });
 });
